e29aff85df5df71269488ff6db01ffd218f79269
Sarah Kain <sarahvkain@gmail.com>
2020-08-27 19:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const theBeatlesPlay = (musicians, instruments) => {
   const newArr = [];
-  const musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
-  const instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
   for(let i = 0; i <= musicians.length-1; i++){
     let string = `${musicians[i]} plays ${instruments[i]}`
     newArr.push(string)
