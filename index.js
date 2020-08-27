d59d56bbc79e086a2d6de361f8a19f6f18ebfca3
Sarah Kain <sarahvkain@gmail.com>
2020-08-27 19:31:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let theBeatlesPlay = (musicians, instruments) => {
-  let 
+const theBeatlesPlay = (musicians, instruments) => {
+  const newArr = [];
+  const musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+  const instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+  for(let counter = 0; counter <= musicians.length-1; counter++){
+    let string = `${musicians[counter]
+  }
 }
 
